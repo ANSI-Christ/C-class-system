825a5d91b908e87fc4e308a273b80fd7692b791c
Klaus <83176613+ANSI-Christ@users.noreply.github.com>
2024-06-18 20:07:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,73 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "class.h"
 
 
-void main(){
 
+#define CLASS_BEGIN__A \
+    public(\
+        void(*f)(void*);\
+        char *s;\
+    ),\
+    private(\
+        int bla;\
+    )
+CLASS_END(A);
+
+void f1(CLASS A *p){
+   printf("%s (%d)\n",p->s,p->bla); 
+}
+
+CLASS_COMPILE(A)(
+    constructor(
+        self->f=(void*)f1;
+        self->s=malloc(2);
+        self->s[1]=0;
+        self->s[0]='$';
+        self->bla=5;
+    ),
+    destructor(
+        free(self->s); self->s=NULL;\
+    )
+)
+
+
+
+#define CLASS_BEGIN__B \
+    from(A),\
+    constructor(\
+        self->f=(void*)f2;\
+        self->s[0]='@';\
+    ),\
+    public(\
+        void *b;\
+    )
+CLASS_END(B);
+
+void f2(CLASS B *p){
+   printf("%s\n",p->s); 
+}
+
+CLASS_COMPILE(B)()
+
+
+
+  
+int main()
+{
+    {
+        CLASS A c;
+        A()->constructor(&c);
+        c.f(&c);
+        c.destructor(&c);
+    }
+    
+    {
+        CLASS A c;
+        B()->constructor(&c);
+        c.f(&c);
+        c.destructor(&c);
+    }
+
+    return 0;
 }
