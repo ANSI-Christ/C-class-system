6e5bbb0abeba4269c400a568ede0bb1402525b60
Klaus <direjor2010@yandex.ru>
2024-07-17 02:09:32 +0300

    update
    Rewrite example and add som doc.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-C class system via macro
+# C class system
+
+class.h allow to create classes in C.
+
+main.c contains 4 classes example and main.E contains code after preprocessing main.c.
+
+Keywords:
+ - constructor  (may have parameters)
+ - destructor   (have empty parameters)
+ - extends      (one parameter - )
+ - abstract     ()
+ - public
+ - private      (invisible in childs)
+
+All keywords are optional.
+
+Creation starts with defining:  #define CLASS_BEGIN__someName keyword,keyword,keyword
+
+Then type must be genereted with calling: CLASS_END(someName);
+
+Class can be compilled by calling:  CLASS_COMPILE(someName)(keyword, ... )
+Its unnesessary if you want only fields inheritance.
+CLASS_COMPILE know only constructor & destructor keywords.
+
+constructor & destructor are consist of two parts: visible to user (by CLASS_BEGIN__someName) and hide (by CLASS_COMPILE).
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -4,31 +4,48 @@
 
 
 
-#define CLASS_BEGIN__A \
-    constructor(int a)(),\
+#define CLASS_BEGIN__X abstract,\
+    constructor(int a)(\
+        self->a=a;\
+    ),\
     public(\
+        int a;\
         void(*f)(void*);\
+    )
+CLASS_END(X);
+
+CLASS_COMPILE(X)()
+
+
+
+#define CLASS_BEGIN__A \
+    extends(X),\
+    constructor(int a,char b,double c,char *s)(),\
+    public(\
+        char b;\
+        double c;\
         char *s;\
     ),\
     private(\
-        int bla;\
+        int x;\
     )
 CLASS_END(A);
 
 void f1(CLASS A *p){
-   printf("%s (%d)\n",p->s,p->bla); 
+   printf("%d %c %f %s (%d)\n",p->a,p->b,p->c,p->s,p->x);
 }
 
 CLASS_COMPILE(A)(
     constructor()(
+        super(self,a);
         self->f=(void*)f1;
-        self->s=malloc(2);
-        self->s[1]=0;
-        self->s[0]='#';
-        self->bla=a;
+        self->b=b;
+        self->c=c;
+        self->s=s;
+        self->x=a+b;
     ),
     destructor()(
-        free(self->s); self->s=NULL;\
+        if(self->s) free(self->s); self->s=NULL;\
     )
 )
 
@@ -36,54 +53,81 @@ CLASS_COMPILE(A)(
 
 #define CLASS_BEGIN__B \
     extends(A),\
-    constructor(char c)(\
-        super(self,5);\
+    constructor(int a,char b,double c,char *s,int cnt)(\
+        super(self,999,'$',c,s);\
         self->f=(void*)f2;\
-        self->s[0]=c;\
+        self->arr=malloc(sizeof(*self->arr)*cnt);\
+    ),\
+    destructor()(\
+        if(self->arr) free(self->arr); self->arr=NULL;\
     ),\
     public(\
-        void *b;\
+        int *arr;\
     )
 CLASS_END(B);
 
 void f2(CLASS B *p){
-   printf("%s\n",p->s); 
+    A()->f(p);
+    printf("%p\n",p->arr);
 }
 
 CLASS_COMPILE(B)()
 
 
 
+#define CLASS_BEGIN__C abstract,\
+    extends(A),\
+    constructor()(\
+        super(self,0,0,0,NULL);\
+    )
+CLASS_END(C);
+
+CLASS_COMPILE(C)()
+
+
   
-int main()
+void main()
 {
+    /*  CLASS X is abstract and constructor doesnt exist at compilation
+        CLASS X c;
+        X()->constructor(&c);
+        c.f(&c);
+        c.destructor(&c);
+    */
+    
     {
+        char *s=malloc(10);
+        sprintf(s,"1111");
         CLASS A c;
-        A()->constructor(&c,4);
+        A()->constructor(&c,1,'1',1.1,s);
         c.f(&c);
         c.destructor(&c);
     }
     
     if(0){
+        char *s=malloc(10);
+        sprintf(s,"1111");
         CLASS A c;
-        B()->constructor(&c,'$'); // out of bounds sizeof(A) < sizeof(B)
+        B()->constructor(&c,2,'2',2.2,s,20); // out of bounds sizeof(A) < sizeof(B)
         c.f(&c);
         c.destructor(&c);
     }
     
     {
+        char *s=malloc(10);
+        sprintf(s,"3333");
         CLASS B c;
-        B()->constructor(&c,'$');
+        B()->constructor(&c,3,'3',3.3,s,30);
         c.f(&c);
         c.destructor(&c);
     }
 
     {
-        CLASS A *c=B()->constructor(malloc(sizeof(CLASS B)),'%');
+        char *s=malloc(10);
+        sprintf(s,"4444");
+        CLASS A *c=B()->constructor(malloc(sizeof(CLASS B)),4,'4',4.4,s,40);
         c->f(c);
         c->destructor(c);
         free(c);
     }
-
-    return 0;
 }
